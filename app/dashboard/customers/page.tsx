7c18e4b57bec9934679c0e649987f55a75f9a332
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-08-31 15:50:17 +0200

    init repo

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+// type Props = {}
+
+export default function Page() {
+  return <p>Customers Page</p>;
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+// type Props = {}
+
+export default function Page() {
+  return <p>This is invoices page</p>;
+}
